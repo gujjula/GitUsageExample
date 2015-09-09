7d830aa70708f5ab7b4730a4874f6fdaf42497b4
Natsu Dragneel <hg2167@gmail.com>
2015-09-09 14:35:29 +0530

    Initial Commit

---
+++ GitUsageExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitUsageExample
+//
+//  Created by Natsu Dragneel on 09/09/15.
+//  Copyright (c) 2015 Harsha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitUsageExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitUsageExample
+//
+//  Created by Natsu Dragneel on 09/09/15.
+//  Copyright (c) 2015 Harsha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
