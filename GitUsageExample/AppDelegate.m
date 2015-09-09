2dd1f881bccec02b71f03874256eba1cbf217e4e
Natsu Dragneel <hg2167@gmail.com>
2015-09-09 14:41:22 +0530

    New Branch added

--- GitUsageExample/AppDelegate.m
+++ GitUsageExample/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 	// Override point for customization after application launch.
 	NSLog(@"App delegate");
+	NSLog(@"Branch App delegate");
 	return YES;
 }
 
